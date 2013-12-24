d1f9fee76512732d58cf53b0b4cd0dbefb9c710e
Iwan Gabovitch <qubodup@gmail.com>
2013-12-25 00:11:03 +0100

    0.9 fix

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 ]]--
 
 function love.load()
-  success = love.graphics.setMode(800,600)
+  success = love.window.setMode(800,600)
 
   gfx = {
     bothalive = love.graphics.newImage("gfx/bothalive.png"),
